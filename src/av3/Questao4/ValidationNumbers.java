fe6b21b4f353c4f3e7b3313bb080c521ca88e02a
JotaT <85948500+JNathan17@users.noreply.github.com>
2021-12-03 23:03:02 -0300

    Add files via upload

---
+++ src/av3/Questao4/ValidationNumbers.java
@@ -0,0 +1,12 @@
+
+package av3.Questao4;
+
+public class ValidationNumbers {
+    
+    public boolean max(int a, int b){
+        if(a > b){
+            return true;
+        }      
+            return false; 
+    }
+}
